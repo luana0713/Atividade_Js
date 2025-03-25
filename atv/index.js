29cd3137642b5250bcca7e128cb806e2a150e57c
luana0713 <166567157+luana0713@users.noreply.github.com>
2025-03-24 21:14:43 -0300

    Adicionando o arquivo index.js

---
+++ atv/index.js
@@ -0,0 +1,24 @@
+
+
+let nome= "luana";
+let idade= 19;
+let ehEstudante= true;
+
+
+
+let numero =5;
+let texto = "hello,guys";
+let booleano = true;
+let array =[1,2,3];
+let objeto ={Nome: "luana", Idade: "19"};
+
+
+
+ console.log("Nome", nome);
+ console.log("Idade", idade);
+console.log("let numero", typeof numero);
+console.log("let texto", typeof texto);
+console.log("let booleano", typeof booleano);
+console.log("let array", typeof array);
+console.log("let objeto", typeof objeto);
+
